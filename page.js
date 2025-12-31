b3f32217519c7e6939feabc9bf7f7f288b498167
chadhagerty <chadhagerty@gmail.com>
2025-12-31 08:58:10 -0500

    Create Home component for Disc Golf page

---
+++ page.js
@@ -0,0 +1,8 @@
+default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>In The Pitts Disc Golf</h1>
+      <p>Course check-in coming soon.</p>
+    </main>export
+  );
+}
